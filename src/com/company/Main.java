3b437525bead679af5f47d5f6c7cd36f4e7a3ec9
Selmeczi Tamas <dizsit@gmail.com>
2017-08-09 15:14:27 +0200

    create currencyConverter class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,13 @@ public class Main {
         showMainMenu();
     }
 
+    public static int input() {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Please Enter a number: ");
+        int inputNumber = reader.nextInt();
+        return inputNumber;
+    }
+
     public static void showMainMenu() {
 
         System.out.println("");
@@ -19,13 +26,53 @@ public class Main {
         System.out.println("(3) Weight converter");
         System.out.println("(4) Temperature converter");
         System.out.printf("");
-
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Please Enter a number: ");
-        int number = reader.nextInt();
-        System.out.println("You choose number " + number);
-        chooseMenu(number);
+        chooseMenu(input());
     }
 
+    public static void chooseMenu(int number) {
+        switch (number) {
+            case 1:
+                System.out.printf("Currency converter menu");
+                System.out.println("");
+                System.out.println("Please choose from the following options:");
+                System.out.println("(1) HUF to EUR");
+                System.out.println("(2) EUR to HUF");
+                System.out.println("(3) HUF to USD");
+                System.out.println("(4) Check the daily exchange rate");
+                System.out.println("(0) Go back to Main Menu");
+                // currencyConverter(input());
 
+                break;
+            case 2:
+                System.out.println("Distance converter menu");
+                break;
+            case 3:
+                System.out.println("Weight converter menu");
+                break;
+            case 4:
+                System.out.println("Temperature converter menu");
+                break;
+            default:
+                System.out.println("Please choose from option: 1,2,3 or 4");
+                showMainMenu();
+                break;
+
+        }
+    }
+
+    public static void currencyConverter(int number) {
+        switch (number) {
+            case 1:
+                System.out.println("You are in the HUF to EUR exchanger menu");
+                System.out.printf("");
+                System.out.println("Please enter your amount of HUF");
+                int hufAmount = input();
+                double euroAmount = (double) hufAmount / 300;   // 300 should be a variable (euroToday)
+                System.out.println(hufAmount + " HUF today equals with " + euroAmount + " EUR");
+                break;
+        }
+    }
+
+    currencyConverter hufToEuro = new currencyConverter();
+    hufToEuro.setHuf(100000);
 }
---
+++ src/com/company/currencyConverter.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class currencyConverter {
+
+    private double huf;
+    private double eur;
+    private double exchangeRate;
+    private double result;
+
+    public double getHuf() {
+        return huf;
+    }
+
+    public void setHuf(double huf) {
+        this.huf = huf;
+    }
+
+    public double getEur() {
+        return eur;
+    }
+
+    public void setEur(double eur) {
+        this.eur = eur;
+    }
+
+    public double getExchangeRate() {
+        return exchangeRate;
+    }
+
+    public void setExchangeRate(double exchangeRate) {
+        this.exchangeRate = exchangeRate;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    public void setResult(double result) {
+        this.result = result;
+    }
+}
