0077d34930d3818009e927e653a40f29034c9439
Selmeczi Tamas <dizsit@gmail.com>
2017-08-09 12:55:50 +0200

    init showMainMenu method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
+import java.util.*;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to Javaica Converter");
+        showMainMenu();
     }
+
+    public static void showMainMenu() {
+
+        System.out.println("");
+        System.out.println("You can choose from the following options:");
+        System.out.println("(1) Currency converter");
+        System.out.println("(2) Distance converter");
+        System.out.println("(3) Weight converter");
+        System.out.println("(4) Temperature converter");
+        System.out.printf("");
+
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Please Enter a number: ");
+        int number = reader.nextInt();
+        System.out.println("You choose number " + number);
+        chooseMenu(number);
+    }
+
+
 }
