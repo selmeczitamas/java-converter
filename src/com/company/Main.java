17cda0c08d09dbabce043de1a6da37eefaaa9750
Selmeczi Tamas <dizsit@gmail.com>
2017-08-10 13:51:20 +0200

    Add some design to printings.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,9 @@ import com.sun.org.apache.xpath.internal.SourceTree;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("----------------------------------------");
         System.out.println("Welcome to Javaica Converter Application");
+        System.out.println("----------------------------------------");
         showMainMenu();
     }
 
@@ -17,8 +19,10 @@ public class Main {
         try {
             inputNumber = reader.nextInt();
         } catch (InputMismatchException a) {
+            System.out.println("----------------------------------------");
             System.out.println("Error. I'm waiting for a number.");
             System.out.println("You transferred back to Main menu.");
+            System.out.println("----------------------------------------");
             showMainMenu();
         }
         return inputNumber;
@@ -58,11 +62,16 @@ public class Main {
                 break;
 
             case 0:
-                System.out.println("See you!");
+                System.out.println("----------------------------------------");
+                System.out.println("See you! Bye! :)");
+                System.out.println("----------------------------------------");
+                System.exit(0);
                 break;
 
             case 666:
+                System.out.println("----------------------------------------");
                 System.out.println("WHO ARE YOU???");
+                System.out.println("----------------------------------------");
                 showMainMenu();
                 break;
 
@@ -74,72 +83,84 @@ public class Main {
     }
 
     public static void currencyConverterMenu() {
-        System.out.println("");
-        System.out.printf("Currency converter menu");
+        System.out.println("----------------------------------------");
+        System.out.println("Currency converter menu");
+        System.out.println("----------------------------------------");
         System.out.println("");
         System.out.println("Please choose from the following options:");
         System.out.println("(1) Convert HUF to EUR");
         System.out.println("(2) Convert EUR to HUF");
         System.out.println("(3) Check the daily exchange rate");
         System.out.println("(0) Go back to Main menu");
+        System.out.println("");
         moneyExchanger(input("Please Enter a number: "));
     }
 
     public static void distanceConverterMenu() {
-        System.out.println("");
+        System.out.println("----------------------------------------");
         System.out.println("Distance converter menu");
+        System.out.println("----------------------------------------");
         System.out.println("");
         System.out.println("Please choose from the following options:");
         System.out.println("(1) Convert km to miles");
         System.out.println("(2) Convert miles to km");
         System.out.println("(0) Go back to Main menu");
+        System.out.println("");
     }
 
     public static void weightConverterMenu() {
-        System.out.println("");
+        System.out.println("----------------------------------------");
         System.out.println("Weight converter menu");
+        System.out.println("----------------------------------------");
         System.out.println("");
         System.out.println("Please choose from the following options:");
         System.out.println("(1) Convert kg to lbs");
         System.out.println("(2) Convert lbs to kg");
         System.out.println("(0) Go back to Main menu");
+        System.out.println("");
     }
 
     public static void temperatureConverterMenu() {
-        System.out.println("");
+        System.out.println("----------------------------------------");
         System.out.println("Temperature converter menu");
+        System.out.println("----------------------------------------");
         System.out.println("");
         System.out.println("(1) Convert Celsius to Fahrenheit");
         System.out.println("(2) Convert Fahrenheit to Celsius");
         System.out.println("(0) Go back to Main menu");
+        System.out.println("");
     }
 
     public static void moneyExchanger(int exChangeableValue) {
         String printOutMsg;
-        double napiEuroVetel = 304.61;
-        double napiEuroEladas = 305.35;
+        double dailyEuroRate = 304.61;
         currencyConverter changeMe = new currencyConverter();
         switch (exChangeableValue) {
 
             case 1:
-                changeMe.setExchangeRate(napiEuroVetel);
+                changeMe.setExchangeRate(dailyEuroRate);
                 printOutMsg = "Please Enter the amount of money in HUF that you want to exchange to EUR: ";
                 changeMe.setHuf(input(printOutMsg));
-                System.out.println(changeMe.getHuf() + " Forint a mai arfolyamon " + (changeMe.getHuf() / changeMe.getExchangeRate()) + " Euro-t er.");
+                System.out.println("----------------------------------------");
+                System.out.println(changeMe.getHuf() + " Forint equals to " + (changeMe.getHuf() / changeMe.getExchangeRate()) + " Euro.");
+                System.out.println("----------------------------------------");
                 showMainMenu();
                 break;
 
             case 2:
-                changeMe.setExchangeRate(napiEuroVetel);
+                changeMe.setExchangeRate(dailyEuroRate);
                 printOutMsg = "Please Enter the amount of money in EUR that you want to exchange to HUF: ";
                 changeMe.setEur(input(printOutMsg));
-                System.out.println(changeMe.getEur() + " Euro a mai arfolyamon " + (changeMe.getEur() * changeMe.getExchangeRate()) + " Forintot er.");
+                System.out.println("----------------------------------------");
+                System.out.println(changeMe.getEur() + " Euro equals to " + (changeMe.getEur() * changeMe.getExchangeRate()) + " HUF.");
+                System.out.println("----------------------------------------");
                 showMainMenu();
                 break;
 
             case 3:
-                System.out.println("Az aktualis Euro veteli arfolyam: " + napiEuroVetel);
-                System.out.println("Az aktualis Euro eladasi arfolyam: " + napiEuroEladas);
+                System.out.println("----------------------------------------");
+                System.out.println("The current Euro exchange rate is: " + dailyEuroRate);
+                System.out.println("----------------------------------------");
                 currencyConverterMenu();
                 break;
 
@@ -152,19 +173,4 @@ public class Main {
                 break;
         }
     }
-//    public static void currencyConverter(int number) {
-//        switch (number) {
-//            case 1:
-//                System.out.println("You are in the HUF to EUR exchanger menu");
-//                System.out.printf("");
-//                System.out.println("Please enter your amount of HUF");
-//                int hufAmount = input();
-//                double euroAmount = (double) hufAmount / 300;   // 300 should be a variable (euroToday)
-//                System.out.println(hufAmount + " HUF today equals with " + euroAmount + " EUR");
-//                break;
-//        }
-//    }
-//
-//    currencyConverter hufToEuro = new currencyConverter();
-//    hufToEuro.setHuf(100000);
 }
